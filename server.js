a3b7e8cf1986c465219ef40855a107b83f6595c1
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-04-17 19:53:10 +0100

    Add post('/api/foods') to server.js

--- server.js
+++ server.js
@@ -6,6 +6,37 @@ server.use(parser.json());
 server.use(express.static('client/build'));
 server.use(parser.urlencoded({extended: true}));
 
-server.listen(3000, function(){
-  console.log("Listening on port 3000");
-});
+const MongoClient = require('mongodb').MongoClient;
+
+
+MongoClient.connect('mongodb://localhost:27017', function(err, client){
+  if(err){
+    console.log(err);
+    return;
+  }
+
+  const db = client.db("favourite_foods");
+
+  console.log('Connected to database');
+
+  // create route
+  server.post('/api/foods', function(req, res){
+    const foodsCollection = db.collection('favouriteFoods');
+    const foodToSave = req.body;
+    foodsCollection.save(foodToSave, function(err, result){
+      if(err){
+        console.log(err);
+        res.status(500);
+        res.send();
+      }
+      console.log('saved to database');
+      res.status(201);
+      res.json(foodToSave);
+    });
+  });
+
+
+  server.listen(3000, function(){
+    console.log("Listening on port 3000");
+  });
+})
